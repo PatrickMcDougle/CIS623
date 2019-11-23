92169ae53ba45b74b7d592a3ab22e02fba3fa21a
Patrick McDougle <Patrick.McDougle@gmail.com>
2019-11-23 01:28:30 -0700

    Initial Commit
    Initial commit with some work on the UI side of the application.

---
+++ PatrickMcDougle_CTL_Star/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace PatrickMcDougle_CTL_Star
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
---
+++ PatrickMcDougle_CTL_Star/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace PatrickMcDougle_CTL_Star
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+
+			var viewModel = new ModelViewModel
+			{
+				NumberOfStates = 4
+			};
+
+			DataContext = viewModel;
+
+			InitializeComponent();
+		}
+	}
+}
